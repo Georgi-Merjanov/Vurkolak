335c035d2f2da9436988af577d543b0970d98a97
Georgi-Merjanov <georgi.merdjanov10@gmail.com>
2026-07-07 16:45:30 +0300

    types.h

--- types.h
+++ types.h
@@ -1,10 +1,44 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#define MAX_NAME 50
+#define MAX_ROLE_NAME 20
+#define MAX_DESCRIPTION 500
+
 typedef enum {
-    GOOD=1, BAD, SOLO, UNKNOWN
+    GOOD = 1,  // От добрите (селото)
+    BAD,       // От лошитр (главатар и вълци)
+    SOLO,      // Мазохист
+    UNKNOWN    // Още не се знае, защото е Крадец или Двоен Агент
 } Team;
 
+typedef enum {
+    THIEF = 1,     // Крадец
+    DOUBLE_AGENT,  // Двоен агент
+    LEADER,        // Главатар
+    WOLF,          // Вълк
+    SLEEPLESS,     // Безсънница
+    SEER,          // Гадател
+    DOCTOR,        // Лекар
+    ASSASSIN,      // Убиец
+    MASOCHIST,     // Мазохист
+    VILLAGER       // Селянин
+} Role_type;
 
+typedef struct {
+    Role_type type; // Номер на ролята за по-бързо изчисляване
+    char name[MAX_ROLE_NAME]; // Име на ролята
+    char description[MAX_DESCRIPTION]; // Обяснение на ролята
+    Team team; // От кой отбор е
+} Role;
+
+typedef struct {
+    char name[MAX_NAME];  // Името на играча
+    Role role;            // Ролята на играча
+    Team team;            // Отборът на играча
+    int is_alive;         // 1 = жив, 0 = мъртъв
+    int is_dvoen_agent;   // 1 = Двоен агент, 0 = не е
+    int is_kmet;          // 1 = Кмет (има решаващ глас), 0 = не е
+} Player;
 
 #endif
